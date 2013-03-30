0904b105eac738075a9c32fcfb580929d90f2911
Mohit Thatte <mohit.thatte@thoughtworks.com>
2013-03-30 10:04:15 +0000

    Mohit : Starting a Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KrantiWeb::Application.config.secret_token = '2cc86606d58949fe46834059f121564276525e3f7151a9e46b50c21e4326eaf492b1d57da2db9ae261110b135beb18ec5536a2ce9e9accdd13a0ad86b659a785'
